dafa2aac6e22cd277b6210957bde3b28f9d45bfa
Daniel <danielRenteria@uvicate.com>
2020-04-16 19:11:16 -0600

    Connected to atlas db

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ if config_decouple('PRODUCTION', default=False):
 
 app = create_app(enviroment)
 
-connect('mongodb://localhost:27017/ezECG')
+# connect('mongodb://localhost:27017/ezECG')
+connect('mongodb://danielRenteria22:3838380814@cluster0-shard-00-00-wtqqx.mongodb.net:27017/mydb,cluster0-shard-00-01-wtqqx.mongodb.net:27017/mydb,cluster0-shard-00-02-wtqqx.mongodb.net:27017/mydb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
 ecgModel = tf.keras.models.load_model('ecg.model')
 labels = ['Non-ecotic beats (normal beat)', 
 'Supraventricular ectopic beats',
